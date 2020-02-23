2522d73849d0ce1cea96015e8beeff191892b187
Vicky Yu <vicky@vickys-air.lan>
2020-02-23 11:39:00 +0800

    refactor codes

--- article/article.go
+++ article/article.go
@@ -1,9 +1,75 @@
 package article
 
+import (
+	//Go comes with some excellent support for encoding and decoding these formats using the standard library package, encoding/json.
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 // Article - struct for all articles
 type Article struct {
 	ID      string `json:"ID"`
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
+}
+
+// Articles - a global Articles array
+var Articles []Article
+
+// IndexPage - function homePage
+func IndexPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the IndexPage!") //Fprintf formats according to a format specifier
+	fmt.Println("Endpoint Hit: indexPage")      //Println formats using the default formats
+}
+
+//ReturnAllArticles -  function Return All Articles
+func ReturnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles) // NewEncoder returns a new encoder that writes to w.
+}
+
+// ReturnSingleArticle - function Return Single Article
+func ReturnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Vars returns the route variables for the current request, if any.
+	fmt.Println("route variables", vars)
+	key := vars["id"]
+
+	for _, article := range Articles {
+		if article.ID == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+// CreateNewArticle - function Create New Article
+func CreateNewArticle(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// unmarshal this into a new Article struct
+	// append this to our Articles array.
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	// update our global Articles array to include
+	// our new Article
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+// DeleteArticle - function Delete Article by id
+func DeleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for index, article := range Articles {
+		if article.ID == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+
 }
--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	//Go comes with some excellent support for encoding and decoding these formats using the standard library package, encoding/json.
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+
 	"log"
 	"net/http"
 
@@ -13,67 +11,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//Article - defined in article package
+// Article - defined in article package
 type Article = article.Article
 
 // Articles - a global Articles array
 var Articles []Article
 
-func indexPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the IndexPage!") //Fprintf formats according to a format specifier
-	fmt.Println("Endpoint Hit: indexPage")      //Println formats using the default formats
-}
-
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles) // NewEncoder returns a new encoder that writes to w.
-}
-
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r) // Vars returns the route variables for the current request, if any.
-	fmt.Println("route variables", vars)
-	key := vars["id"]
-
-	for _, article := range Articles {
-		if article.ID == key {
-			json.NewEncoder(w).Encode(article)
-		}
-	}
-}
-
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
-	// get the body of our POST request
-	// unmarshal this into a new Article struct
-	// append this to our Articles array.
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var article Article
-	json.Unmarshal(reqBody, &article)
-	// update our global Articles array to include
-	// our new Article
-	Articles = append(Articles, article)
-
-	json.NewEncoder(w).Encode(article)
-}
-
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	for index, article := range Articles {
-		if article.ID == id {
-			Articles = append(Articles[:index], Articles[index+1:]...)
-		}
-	}
-
-}
-
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", indexPage)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/", article.IndexPage)
+	myRouter.HandleFunc("/articles", article.ReturnAllArticles)
+	myRouter.HandleFunc("/article", article.CreateNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", article.DeleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", article.ReturnSingleArticle)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
